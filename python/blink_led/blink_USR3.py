9608664ff5c42971c745324c8fbf2289ec6b81cf
nnome <90666298+nnome@users.noreply.github.com>
2021-10-03 21:35:27 -0500

    Added blink_led code

---
+++ python/blink_led/blink_USR3.py
@@ -0,0 +1,10 @@
+import Adafruit_BBIO.GPIO as GPIO
+import time
+
+GPIO.setup("USR3", GPIO.OUT)
+
+while True:
+    GPIO.output("USR3", GPIO.HIGH)
+    time.sleep(1/10)
+    GPIO.output("USR3", GPIO.LOW)
+    time.sleep(1/10)
